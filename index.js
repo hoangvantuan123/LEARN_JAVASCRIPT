eb675410b37130694c652ce2be367276e6b0e1e0
tuanhoang <rammatuan@gmail.com>
2021-09-16 22:09:37 +0700

    Learn JS

---
+++ index.js
@@ -0,0 +1,9 @@
+function myFunction(){
+    document.getElementById('demo5').style.color ='red';
+}
+const cars= ['saab ','volovo' , 'BMW' ] ;
+cars[0] = 'toyota';
+
+cars.push('audi');
+console.log (cars);
+
