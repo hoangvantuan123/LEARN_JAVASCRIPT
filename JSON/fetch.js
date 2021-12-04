dae63edbbbee091446b272102285ee1339468376
tuanhoang <rammatuan@gmail.com>
2021-12-04 09:29:15 +0700

    add

--- JSON/fetch.js
+++ JSON/fetch.js
@@ -54,19 +54,18 @@ fetch(commit)
 var postAPI ='http://localhost:3000/posts' ;
 fetch(postAPI)
     .then(function (response) {
-        return response.json() ;
+        return response.json();
     })
-    .then(function (posts){
-        var htmls = posts.map(function (post){
-            return `<li>
-                <h2>${post.title}</h2>
-                <h2>${post.author}</h2>
-                <h2>${post.id}</h2>
+    .then(function (posts) {
+        var htmls = posts.map(function (posts){
+            return ` <li>
+                <h2>${posts.id}</h2>
+                <h2>${posts.title}</h2>
             </li>`
         })
         var html = htmls.join('')
-        document.getElementById('post-block').innerHTML  = html ;
+        document.getElementById('post-block').innerHTML =html ;
     })
     .catch(function (error) {
-        console.log('err')
+        console.log('co loi ');
     })
